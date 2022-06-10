cf286fe4158d88d4747cb097941b170ae79da404
Ludmilla Carvalho <contato@devlud.com.br>
2022-06-10 18:09:14 -0300

    ajuste em contato

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -15,14 +15,14 @@
         <div>
             <span class="inline-flex items-baseline">
                 <img src="{{ asset('images/icons/envelope.svg') }}" alt="" class="self-center w-4 h-4 sm:w-6 sm:h-6 mr-0.5 sm:mr-1 icon-envelope" />
-                <span class="text-xs sm:text-base">lorenipsulon@lorenloripsulone.com.br</span>
+                <span class="text-xs sm:text-base">contato@ftestrategias.com.br</span>
             </span>
         </div>
 
         <div>
             <span class="inline-flex items-baseline sm:mt-2">
                 <img src="{{ asset('images/icons/phone.svg') }}" alt="" class="self-center w-4 h-4 sm:w-6 sm:h-6 mr-0.5 sm:mr-1 icon-phone" />
-                <span class="text-xs sm:text-base">11 0000 0000</span>
+                <span class="text-xs sm:text-base">11 94064-7148</span>
             </span>
         </div>
     </div>
@@ -40,14 +40,14 @@
             <div>
                 <span class="inline-flex items-baseline">
                     <img src="{{ asset('images/icons/envelope.svg') }}" alt="" class="self-center w-8 h-8 mr-2 icon-envelope" />
-                    <span class="text-lg">lorenipsulon@lorenloripsulone.com.br</span>
+                    <span class="text-lg">contato@ftestrategias.com.br</span>
                 </span>
             </div>
 
             <div>
                 <span class="inline-flex items-baseline mt-4">
                     <img src="{{ asset('images/icons/phone.svg') }}" alt="" class="self-center w-8 h-8 mr-2 icon-phone" />
-                    <span class="text-lg">11 0000 0000</span>
+                    <span class="text-lg">11 94064-7148</span>
                 </span>
             </div>
         </div>
@@ -57,10 +57,14 @@
     <div class="my-4">
         <span class="inline-flex items-center">
             <span class="text-xs sm:text-sm md:text-base lg:text-lg">Acesse nossas redes sociais: </span>
-            <a href="#" target="_blank"><img src="{{ asset('images/contato/insta.png') }}" alt="Instagram" class="self-center w-8 h-8 mx-2" /></a>
-            <a href="#" target="_blank"><img src="{{ asset('images/contato/face.png') }}" alt="Facebook" class="self-center w-8 h-8 mx-2" /></a>
-            <a href="#" target="_blank"><img src="{{ asset('images/contato/yt.png') }}" alt="You Tube" class="self-center w-8 h-8 mx-2" /></a>
-            <a href="#" target="_blank"><img src="{{ asset('images/contato/tw.png') }}" alt="Twitter" class="self-center w-8 h-8 mx-2" /></a>
+            <a href="https://www.instagram.com/risadaria" target="_blank"><img src="{{ asset('images/contato/insta.png') }}" alt="Instagram" class="self-center w-8 h-8 mx-2" /></a>
+            <a href="https://web.facebook.com/RISADARIA" target="_blank"><img src="{{ asset('images/contato/face.png') }}" alt="Facebook" class="self-center w-8 h-8 mx-2" /></a>
+            <a href="https://www.youtube.com/user/Risadaria" target="_blank"><img src="{{ asset('images/contato/yt.png') }}" alt="Youtube" class="self-center w-8 h-8 mx-2" /></a>
+            <a href="https://twitter.com/RISADARIA" target="_blank"><img src="{{ asset('images/contato/tw.png') }}" alt="Twitter" class="self-center w-8 h-8 mx-2" /></a>
         </span>
     </div>
 </x-site-layout>
+
+
+
+Entre os dias 5 e 14 de julho, o shopping Tietê receberá atrações do Risadaria. O local servirá de palco para  apresentações gratuitas dos grandes nomes do humor. Duração aproximada: 25 minutos Classificação indicativa: 16 anos.
