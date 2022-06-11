caa0a2b2f07cb1dea9b76523d42f8de0c8567958
Ludmilla Carvalho <contato@devlud.com.br>
2022-06-11 11:32:58 -0300

    ajuste na home

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,14 +1,20 @@
 <x-site-layout>
     {{-- Carousel --}}
-    <div id="carouselExampleCaptions" class="carousel slide relative" data-bs-ride="carousel">
+    <div id="carouselExampleCaptions" class="carousel slide carousel-fade class relative" data-bs-ride="carousel">
         <div class="carousel-indicators absolute right-0 -bottom-12 left-0 flex justify-center p-0 mb-4 md:hidden">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
             {{-- <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button> --}}
         </div>
         <div class="carousel-inner relative w-full overflow-hidden">
             <div class="carousel-item active relative float-left w-full">
-                <img src="{{ asset('images/home/b1.jpeg') }}" class="block w-full" alt="..."/>
+                <a href="https://www.sympla.com.br/eventos?s=risadaria&tab=eventos" target="_blank">
+                    <img src="{{ asset('images/home/Supershows.png') }}" class="hidden lg:block w-full" alt="..."/>
+                    <img src="{{ asset('images/home/Supershows_m.png') }}" class="block lg:hidden w-full" alt="..."/>
+                </a>
                 {{-- <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg" class="block w-full" alt="..."/> --}}
                 {{-- <div class="carousel-caption hidden md:block absolute text-center">
                     <h5 class="text-xl">First slide label</h5>
@@ -16,12 +22,29 @@
                 </div> --}}
             </div>
             <div class="carousel-item relative float-left w-full">
-                <img src="{{ asset('images/home/b1.jpeg') }}" class="block w-full" alt="..."/>
-                {{-- <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg" class="block w-full" alt="..."/> --}}
+                <a href="https://www.sympla.com.br/eventos?s=risadaria&tab=eventos" target="_blank">
+                    <img src="{{ asset('images/home/KAMBALACHO.png') }}" class="hidden lg:block w-full" alt="..."/>
+                    <img src="{{ asset('images/home/KAMBALACHO_m.png') }}" class="block lg:hidden w-full" alt="..."/>
+                </a>
             </div>
-            {{-- <div class="carousel-item relative float-left w-full">
-                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg" class="block w-full" alt="..."/>
-            </div> --}}
+            <div class="carousel-item relative float-left w-full">
+                <a href="https://www.sympla.com.br/eventos?s=risadaria&tab=eventos" target="_blank">
+                    <img src="{{ asset('images/home/Supershows_1.png') }}" class="hidden lg:block w-full" alt="..."/>4
+                    <img src="{{ asset('images/home/Supershows_1.png') }}" class="block lg:hidden w-full" alt="..."/>
+                </a>
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <a href="https://www.sympla.com.br/eventos?s=risadaria&tab=eventos" target="_blank">
+                    <img src="{{ asset('images/home/AQUELA-DUPLA.png') }}" class="hidden lg:block w-full" alt="..."/>
+                    <img src="{{ asset('images/home/AQUELA-DUPLA.png') }}" class="block lg:hidden w-full" alt="..."/>
+                </a>
+            </div>
+            <div class="carousel-item relative float-left w-full">
+                <a href="https://www.sympla.com.br/eventos?s=risadaria&tab=eventos" target="_blank">
+                    <img src="{{ asset('images/home/BAD-TRIP.png') }}" class="hidden lg:block w-full" alt="..."/>
+                    <img src="{{ asset('images/home/BAD-TRIP.png') }}" class="block lg:hidden w-full" alt="..."/>
+                </a>
+            </div>
         </div>
         <button class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-cen border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
             <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
