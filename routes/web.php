b85e2921b523a5377d1e2c15dd6c2ae0f19e3ad4
Ludmilla Carvalho <contato@devlud.com.br>
2022-06-13 13:51:33 -0300

    home no ar

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('site.espera');
-})->name('espera');
+// Route::get('/', function () {
+//     return view('site.espera');
+// })->name('espera');
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('site.home');
 })->name('home');
 
